61c1ccaa4a8f04c880b17caf176ae8178bf2f2d0
Beccatoni <rbccmtni1@gmail.com>
2025-04-23 18:10:52 +0200

    add countries and try to remove data

--- Pages/Countries/Index.cshtml.cs
+++ Pages/Countries/Index.cshtml.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using RazorCountry.Data;
 using RazorCountry.Models;
 
-namespace RazorCountry.Pages.Continents;
+namespace RazorCountry.Pages.Countries;
 
 public class Index : PageModel
 {
@@ -15,24 +15,26 @@ public class Index : PageModel
         _context = context;
     }
     
-    public List<Continent> Continents { get; set; }
+    public List<Country> Countries { get; set; }
     
     public async Task OnGetAsync()
     {
-        Continents = await _context.Continents.ToListAsync();
+        Countries = await _context.Countries.ToListAsync();
     }
 
-    public async Task<IActionResult> OnPostAsync(string id)
+    public async Task<IActionResult> OnPostAddCountryAsync(string id)
     {
         if (id == null)
         {
             return NotFound();
         }
-        Continent continent = await _context.Continents.FindAsync(id);
-        if (continent != null)
+        
+        Country country = await _context.Countries.FindAsync(id);
+        if (country != null)
         {
-            _context.Continents.Remove(continent);
+            _context.Countries.Remove(country);
         }
+
         await _context.SaveChangesAsync();
         return RedirectToPage("./Index");
     }
